a2c8d237b60ac90205d3e4cfe95002f09207d584
Saitejad007 <saitejadevineni@gmail.com>
2023-02-01 08:08:16 +0530

    Added req handler.

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -2,12 +2,16 @@ let express = require('express');
 let app = express();
 console.log('Hello World')
 
+app.get('/',((req,res)=>{
+    res.send('Hello Express');
+}));
 
 
 
 
 
 
+app.listen(3000);
 
 
 
