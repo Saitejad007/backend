31261fd78381de6c1eb244b3fa460012e46dac15
Saitejad007 <saitejadevineni@gmail.com>
2023-02-08 10:46:41 +0530

    Added /json route.

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,12 +1,15 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-console.log('Hello World')
+console.log("Hello World");
 
-
-app.get('/',function(req,res){
-    res.sendFile(__dirname + '/views/index.html')
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-app.use('/public',express.static(__dirname + '/public'))
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", function (req, res) {
+  res.json({ message: "Hello json" });
+});
 
 module.exports = app;
