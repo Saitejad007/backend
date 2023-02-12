708c0be296c9eab3c9345f75b5e1d2dee3f8b57b
Sai Teja <saitejadevineni@gmail.com>
2023-02-12 22:27:23 +0530

    Added logger middleware at root level.

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -5,7 +5,7 @@ let app = express();
 console.log("Hello World");
 
 app.use(function middleware(req,res,next){
-  console.log(`${req.method} ${req.path} ${req.ip}`)
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
   next()
 })
 
