0223fb1157d240553105137b468fc4f42304151f
Sai Teja <saitejadevineni@gmail.com>
2023-02-12 21:05:39 +0530

    Added logger middleware.

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -10,6 +10,11 @@ app.get("/", function (req, res) {
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(function(req,res,next){
+  console.log(`${req.method} ${req.path} ${req.ip}`)
+  next()
+})
+
 app.get("/json", function (req, res) {
   res.json({ message: process.env.MESSAGE_STYLE ==='uppercase'?'Hello json'.toUpperCase(): 'Hello json'});
 });
