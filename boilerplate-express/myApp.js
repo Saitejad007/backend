ff2beaf5cb96c5ccaa841c06d3f3e0eb29dc3f5d
Sai Teja <saitejadevineni@gmail.com>
2023-02-12 21:16:50 +0530

    Added logger middleware at root level.

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -4,16 +4,18 @@ let express = require("express");
 let app = express();
 console.log("Hello World");
 
+app.use(function middleware(req,res,next){
+  console.log(`${req.method} ${req.path} ${req.ip}`)
+  next()
+})
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.use('/',function(req,res,next){
-  console.log(`${req.method} ${req.path} ${req.ip}`)
-  next()
-})
+
 
 app.get("/json", function (req, res) {
   res.json({ message: process.env.MESSAGE_STYLE ==='uppercase'?'Hello json'.toUpperCase(): 'Hello json'});
