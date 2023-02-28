fded8172d292aa250177782926e180882a139d8d
Sai Teja <saitejadevineni@gmail.com>
2023-02-28 08:53:53 +0530

    Created time server.

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 
 let express = require("express");
 let app = express();
 console.log("Hello World");
 
-app.use(function middleware(req,res,next){
-  console.log(`${req.method} ${req.path} - ${req.ip}`)
-  next()
-})
+app.use(function middleware(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
@@ -15,10 +15,21 @@ app.get("/", function (req, res) {
 
 app.use("/public", express.static(__dirname + "/public"));
 
-
-
-app.get("/json", function (req, res) {
-  res.json({ message: process.env.MESSAGE_STYLE ==='uppercase'?'Hello json'.toUpperCase(): 'Hello json'});
+app.get("/json", function (req, res, next) {
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE === "uppercase"
+        ? "Hello json".toUpperCase()
+        : "Hello json",
+  });
 });
+
+app.get("/now", function (req, res,next) {
+  req.time = new Date().toString();
+  next();
+},(req, res) => {
+  res.send({
+    time: req.time
+  })});
 
 module.exports = app;
