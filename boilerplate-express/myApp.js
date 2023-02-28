da5e1b6f27c9b8f7304e200b1f2dc082fbf2bad4
Saitejad007 <saitejadevineni@gmail.com>
2023-02-28 10:39:13 +0530

    added accessing route params

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -24,12 +24,22 @@ app.get("/json", function (req, res, next) {
   });
 });
 
-app.get("/now", function (req, res,next) {
-  req.time = new Date().toString();
-  next();
-},(req, res) => {
-  res.send({
-    time: req.time
-  })});
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time,
+    });
+  }
+);
+
+app.get("/:word/echo", function (req, res) {
+  const { word } = req.params;
+  res.json({ echo: word });
+});
 
 module.exports = app;
