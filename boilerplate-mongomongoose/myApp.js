8ca49a1d645b4a3b5520a05312bd5145149db219
Sai Teja <saitejadevineni@gmail.com>
2023-03-05 20:42:01 +0530

    Added person schema.

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, ()=>{console.log('connected')});
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: true
+  },
+  age: Number,
+  favouriteFoods: [string]
+})
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
